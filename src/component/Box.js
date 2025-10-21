caf5d43feab619ede8d35fd7bf1b6ebbb985d4e3
Gona Choi <gorani831@gmail.com>
2025-10-22 03:55:43 +0900

    build success: remove img alt warning

--- src/component/Box.js
+++ src/component/Box.js
@@ -4,7 +4,9 @@ const Box = (props) => {
   return (
     <div className='box'>
         <h1>{props.title}</h1>
-        <img className="item-img" src={props.item && props.item.img} />
+        <img className="item-img"
+        src={props.item && props.item.img}
+        alt={props.item ? props.item.name : "item image"} />
         <h2>WIN</h2>
     
     </div>
